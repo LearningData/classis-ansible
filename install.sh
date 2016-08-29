b576ba0fd0e9067542d6e37ea613cc1597d6ea56
Marius Avram <me@mariusavram.com>
2016-08-29 08:37:13 +0200

    require ansible 1.9

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 echo ""
 echo "Installing Ansible..."
 echo ""
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible-1.9
+sudo apt-get update
 sudo apt-get install ansible
 
 echo ""
